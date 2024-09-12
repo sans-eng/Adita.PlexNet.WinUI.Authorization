75f0b1ec873b1091e24b927f7abc3dc2bfeed57b
Setya Adi Kurnia <setya.adi@weq-tech.com>
2024-09-12 17:13:07 +0700

    Add sample project

---
+++ Adita.PlexNet.WinUI.Authorization.Sample/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Adita.PlexNet.Core.Security.Claims;
+using Adita.PlexNet.Core.Security.Principals;
+using Microsoft.UI.Xaml;
+using System.Threading;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Adita.PlexNet.WinUI.Authorization.Sample
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            //Set thread principal to anonymous before executing any codes
+            Thread.CurrentPrincipal = new ApplicationPrincipal(new ApplicationIdentity());
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+
+        private Window m_window;
+    }
+}
